cef8934ab340bedae51a0fcb048b49936fcaf0b0
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-10-25 23:18:16 +0100

    Add headers folder

--- gap.rb
+++ gap.rb
@@ -11,9 +11,9 @@ class Gap < Formula
   depends_on "readline"
 
   def install
-    system "mkdir gap-headers"
     system "./autogen.sh"
-    system "./configure", "--with-readline=/usr/local/opt/readline", "--includedir=gap-headers"
-    system "make install"
+    mkdir "headers"
+    system "./configure", "--with-readline=/usr/local/opt/readline", "--includedir=#{buildpath}/headers"
+    system "make", "install"
   end
 end
