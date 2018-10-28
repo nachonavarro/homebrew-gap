fb8cfa61db3b1d2ad78e45244c24a8229561af8f
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-10-28 17:09:20 +0100

    Finalize formula

--- gap.rb
+++ gap.rb
@@ -5,18 +5,22 @@ class Gap < Formula
   sha256 "f07e99b37391c9a1fdbe76721327e30b698e4835d088131615a1f6a5cebdf7e0"
   version "4.9.3"
 
-  depends_on "autoconf"
-  depends_on "libtool"
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
+  depends_on "wget" => :build
   depends_on "gmp"
   depends_on "readline"
 
   def install
     system "./autogen.sh"
     system "./configure", "--with-readline=/usr/local/opt/readline", "--prefix=#{prefix}"
-    system "make"
-    libexec.install Dir["*"]
-    cd libexec/"bin" do
-      bin.install_symlink File.expand_path(`readlink -n gap.sh`) => "gap"
+    inreplace "bin/gap.sh" do |s|
+      s.gsub! /GAP_DIR=\$\(cd "(.*)"/, "GAP_DIR=$(cd \"#{libexec}\""
+      s.gsub! /GAP_EXE=.*/, "GAP_EXE=#{libexec}"
     end
+    system "make", "-j4"
+    system "make", "bootstrap-pkg-full"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"gap"
   end
 end
